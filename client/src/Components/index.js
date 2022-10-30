1796821760e1ae316e83152c39142db929d98757
Chanvitha Praveen <chanvithapraween@gmail.com>
2022-10-30 21:51:34 +0530

    Latest

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -7,3 +7,4 @@ export { default as Navbar } from './Navbar';
 export { default as Services } from './Services'; 
 export { default as Transactions } from './Transactions'; 
 export { default as Welcome } from './Welcome'; 
+export {}

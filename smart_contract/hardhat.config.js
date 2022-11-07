51c9ee1451c7d8c1f9de371b40711228aceb317e
Chanvitha Praveen <chanvithapraween@gmail.com>
2022-11-08 01:43:32 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
   networks: {
     goerli: {
       url: 'https://eth-goerli.alchemyapi.io/v2/wc527bDcS2V8z0jNPw-vhTuRqmxtXRmQ',
-      accounts: ['3a6a50d2a2e8982d433871d8107250eb16cf2708943e829ec1e927bbb2acbb4b'],
+      accounts: [''],
     }
   }
 }

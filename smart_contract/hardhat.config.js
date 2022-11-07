ccc7ca2987d85a676f20d432b200b7d2f5568905
Chanvitha Praveen <chanvithapraween@gmail.com>
2022-11-08 01:53:20 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,4 +1,3 @@
-//https://eth-goerli.alchemyapi.io/v2/wc527bDcS2V8z0jNPw-vhTuRqmxtXRmQ
 
 require("@nomiclabs/hardhat-waffle");
 // require("@nomiclabs/hardhat-ethers");
@@ -7,7 +6,7 @@ module.exports = {
   solidity: '0.8.14',
   networks: {
     goerli: {
-      url: 'https://eth-goerli.alchemyapi.io/v2/wc527bDcS2V8z0jNPw-vhTuRqmxtXRmQ',
+      url: '',
       accounts: [''],
     }
   }
